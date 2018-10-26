fa3ee5007d419761720ec51f25bb4d92f814aecb
kaniwar103 <kaniwar103@gamil.com>
2018-10-26 14:53:05 +0200

    homework week1

---
+++ script.js
@@ -0,0 +1,62 @@
+//  part 1
+
+console.log("Hello World"); // Hello world in English
+console.log("Halo wereld"); // Hello world in Dutch
+console.log("Cîhane Silav ");// Hello world in Kurdish
+console.log("مرحبا بالعالم");// Hello world in Arabic
+console.log("Γειά σου Κόσμε");// Hello world in Greek
+
+// part 2
+// console.log('I'm awesome'); get a SyntaxError, the correct way is:
+console.log('I\'m awesome');
+
+// part 3
+
+let x;
+console.log("the value of my variable x will be : 10");
+console.log(x);
+
+ x = 10
+console.log("the value of the variable x will be : 10");
+console.log(x);
+
+// part 4
+
+let y ;
+console.log("the value of my string will be : yes");
+console.log(y);
+ y = ("why not") ;
+console.log("the value of my string will be : why not");
+console.log(y);
+
+// part 5
+
+let z = 7.25;
+console.log(z);
+
+let a = Math.round(7.25);
+console.log(a);
+
+let b = Math.max(z, a);
+console.log(b);
+
+// part 6
+
+let myFavoriteAnimals = [];
+console.log("the value of my Array will be: undefined ");
+console.log(myFavoriteAnimals);
+
+myFavoriteAnimals.push("cheetahs","falcons","horses");
+console.log(myFavoriteAnimals);
+
+myFavoriteAnimals.push("baby pig"); 
+console.log(myFavoriteAnimals);
+
+// part 7
+
+let myString = "this is a test";
+ var l = myString.length;
+ console.log('The length of my string is '+ l + ' ');
+ 
+ //part 8
+ 
